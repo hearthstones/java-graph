5c4a52250d955f765e78860f2a22f2fc356aad25
luchao <1967768885@qq.com>
2021-12-24 16:38:14 +0800

    nio: netty http client.

---
+++ nio/src/main/java/com/summary/nio/client/netty/NettyClientInitializer.java
@@ -0,0 +1,26 @@
+package com.summary.nio.client.netty;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+
+/**
+ * Netty客户端初始化器
+ *
+ * @author luchao
+ * @date 2021/12/24
+ */
+public class NettyClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) {
+        socketChannel.pipeline()
+                // request编码
+                .addLast(new HttpRequestEncoder())
+                // response解码
+                .addLast(new HttpResponseDecoder())
+                // 指定handler
+                .addLast(new NettyClientHandler());
+
+    }
+}
